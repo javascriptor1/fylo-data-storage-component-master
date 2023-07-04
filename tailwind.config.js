0c7e913f950f4ca05e3a5a922daa0d84daeb2e47
Mohammed Fakih <mhd.fakih@gmail.com>
2023-07-04 21:07:34 +0300

    finished mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,16 @@ module.exports = {
         sans: ["Raleway", "sans-serif"],
       },
       colors: {
-        Gradient: "hsl(6, 100%, 80%) to hsl(335, 100%, 65%)",
+        Gradient1: "hsl(6, 100%, 80%) to hsl(335, 100%, 65%)",
         PaleBlue: "hsl(243, 100%, 93%)",
         GrayishBlue: "hsl(229, 7%, 55%)",
         DarkBlue: "hsl(228, 56%, 26%)",
         VeryDarkBlue: "hsl(229, 57%, 11%)",
       },
+      backgroundImage:{
+        'mobile-bg': "url('../images/bg-mobile.png')",
+        'desktop-bg': "url('../images/bg-desktop.png')",
+      }
     },
   },
   plugins: [],
